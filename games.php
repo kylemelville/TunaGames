bc15cf2b52e4823ec2562f400e79149dd6fbe302
Kyle Melville <KPMelville@gmail.com>
2018-02-10 19:55:54 -0600

    Implements game platform populating from database
    I'm not sure how ideal the implementation is. I'm concerned about
    performance when the database would be scaled up. However, I simply
    chose to take the most direct approach at least for now.
    Also I dropped in an example Unity game to see how it would do.

--- games.php
+++ games.php
@@ -1,47 +1,55 @@
 <?php
 	$pageSubTitle = 'Games';
 	include('./includes/header.php');
-	require("../mysqli_connect.php");
+	require("../mysqli_connect.php");	
 	$query = "SELECT id, game_name, description, logo, banner
 		FROM games;";
-	$result = @mysqli_query($dbc, $query);
-	if($result && $result->num_rows > 0) {
+	$gameResult = @mysqli_query($dbc, $query);
+	if($gameResult && $gameResult->num_rows > 0) {
 		$gameList = '<ul id="games">';
-		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-			if(empty($row['banner'])) {
+		while ($gameRow = mysqli_fetch_array($gameResult, MYSQLI_ASSOC)) {
+			if(empty($gameRow['banner'])) {
 				$gameList = $gameList.'<li class="game">';	
 			} else {
-				$gameList = $gameList.'<li class="game" style="background-image: url(./images/games/'.$row['banner'].');">';	
+				$gameList = $gameList.'<li class="game" style="background-image: url(./images/games/'.$gameRow['banner'].');">';	
 			}
 			$gameList = $gameList.'<div class="wrapper">
 				<div class="game-logo">';
-			if(empty($row['logo'])) {
-				$gameList = $gameList.$row['game_name'];
+			if(empty($gameRow['logo'])) {
+				$gameList = $gameList.$gameRow['game_name'];
 			} else {
-				$gameList = $gameList.'<img src="./images/games/'.$row['logo'].'" alt="'.$row['game_name'].'" title="'.$row['game_name'].'" />';
+				$gameList = $gameList.'<img src="./images/games/'.$gameRow['logo'].'" alt="'.$gameRow['game_name'].'" title="'.$gameRow['game_name'].'" />';
 			}
 			$gameList = $gameList.'</div>
 				<div class="game-description">
 					<p>
-						'.$row['description'].'
-					</p>
-					<hr />
+						'.$gameRow['description'].'
+					</p>';
+			$gameID = $gameRow['id'];
+			$query = "SELECT game_id, platform_id, page_url, icon, platform_name
+				FROM platforms p
+				JOIN game_platform gp
+					ON p.id = gp.platform_id
+				WHERE gp.game_id = '$gameID'
+				ORDER BY platform_id ASC;";
+			$platformResult = @mysqli_query($dbc, $query);
+			if($platformResult && $platformResult->num_rows > 0) {
+				$gameList = $gameList.'<hr />
 					<span class="play-on">Play on</span>
-					<ul class="platform-list">
-						<li class="platform">
-							<a href="" >
-								<img src="./images/ui/onlinelogo.png" alt="Online" title="Online" />
+					<ul class="platform-list">';
+				while ($platformRow = mysqli_fetch_array($platformResult, MYSQLI_ASSOC)) {
+					//handle if null to redirect to game specific page. for hosting my own webgl games
+					$gameList = $gameList.'<li class="platform">
+							<a href="'.$platformRow['page_url'].'" >
+								<img src="./images/ui/'.$platformRow['icon'].'" alt="'.$platformRow['platform_name'].'" title="'.$platformRow['platform_name'].'" />
 							</a>
-						</li>
-						<li class="platform">
-							<a href="" >
-								<img src="./images/ui/steamlogo.png" />
-							</a>
-						</li>
-					</ul>							
-				</div>
-			</div>
-		</li>';
+						</li>';
+				}
+			}
+			$gameList = $gameList.'</ul>
+						</div>
+					</div>
+				</li>';
 		}
 		$gameList = $gameList.'</ul>';
 		echo $gameList;
